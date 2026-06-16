60bfb666d46349e595e661925b3f4adf714067a6
Suhana <124507932+Suhananahar1@users.noreply.github.com>
2026-06-16 16:24:07 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,17 @@
 
+pattern = input("Enter pattern: ")
+text = input("Enter text: ")
+d = int(input("d: "))
+
+for i in range(len(text) - len(pattern) + 1):
+
+    substring = text[i:i+len(pattern)]
+
+    mismatch = 0
+
+    for j in range(len(pattern)):
+        if pattern[j] != substring[j]:
+            mismatch += 1
+
+    if mismatch <= d:
+        print(i, end=" ")
